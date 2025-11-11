e5e891b25abe8c460411c457ce4bd23945e04e80
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-11-11 11:50:26 -0500

    Added the rest of the functions

--- calculator.py
+++ calculator.py
@@ -9,6 +9,8 @@ calculator.py
 One function per operation, in order.
 """ 
 
+import math
+
 # First example
 def add(a, b): 
     return a + b
@@ -18,3 +20,17 @@ def sub(a, b):
 
 def mul(a, b):
     return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+def log(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError as e:
+        return f"Error: {e}"
+
+def exp(a, b):
+    return a ** b
